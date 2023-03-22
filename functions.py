f0a69f3f6faa03e81a59fe2103c14347367bdbf4
Raúl de la Fe Robles <rauldelafe2000@hotmail.com>
2023-03-22 10:31:10 +0100

    commit_2

--- functions.py
+++ functions.py
@@ -12,9 +12,25 @@ def read_data(nombre_fichero):
 
             dic_datos.update({"dato"+c:
             {
-                "type": _name,
+                "type": ,
                 "fixed acidity": _adress,
                 "volatile acidity": _phone,
                 "citric acid": _email,
                 "residual sugar": _vip
-            }})
+                "chlorides":
+                "free sulfur dioxide":
+                "total sulfur dioxide":
+                "density":
+                "pH":
+                "sulphates":
+                "alcohol":
+                "quality":
+            },})
+
+
+def split(dic_muestras):
+
+    dic_red = {i:dic_muestras[i] for i in dic_muestras if dic_muestras[i]['type'] == "red"}
+    dic_white = {i:dic_muestras[i] for i in dic_muestras if dic_muestras[i]['type'] == "white"}
+
+    return [dic_red.pop, dic_white]
